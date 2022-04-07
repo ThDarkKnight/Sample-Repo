1e696bb775689cebf851e40b8d429e07ddc670fc
Onurhan Hamzaoğlu <onurhan.hamzaoglu@gmail.com>
2022-04-07 22:42:15 +0300

    Added main function and body.

--- Installation/hello.py
+++ Installation/hello.py
@@ -1,9 +1,13 @@
 import sys
 
+def main():
+    print(sys.version)
+    print(sys.executable)
 
-print(sys.version)
-print(sys.executable)
+    username = "Dark Knight"
+    print("Hello, World!")
+    print(f"Hello, {username}")
 
-username = "Dark Knight"
-print("Hello, World!")
-print(f"Hello, {username}")
+
+if __name__ == "__main__":
+    main()
